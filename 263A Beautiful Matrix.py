dbee3b09bd3de96bc86fcf0c3b74b04a0020905f
Sebastian <Seburath@gmail.com>
2020-12-19 20:37:39 -0800

    Update 263A Beautiful Matrix.py

--- 263A Beautiful Matrix.py
+++ 263A Beautiful Matrix.py
@@ -1,9 +1,8 @@
 def solve(matrix):
     for i, row in enumerate(matrix.split('\n')):
         if '1' in row:
-            x_dist = abs(row.find('1')//2 - 2)
-            y_dist = abs(i - 2)
-            return x_dist + y_dist
+            x, y = abs(i - 2), abs(row.find('1')//2 - 2)
+            return x + y
 
 
 if __name__ == '__main__':
